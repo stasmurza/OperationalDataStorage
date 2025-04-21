dc68422b26188a2dcc5396e25efa6cbd2f62b8d7
stasmurza <stasmurza@gmail.com>
2025-04-21 15:58:01 +0300

    Tests.

--- Tests/OperationalDataStorage.Service.Tests/Tests/ServiceTests.cs
+++ Tests/OperationalDataStorage.Service.Tests/Tests/ServiceTests.cs
@@ -182,19 +182,50 @@ public class ServiceTests
         // Assert.
         positions.Should().NotBeNull();
         positions.Positions.Should().NotBeNull();
+        positions.Positions.Should().HaveCount(0);
+    }
+
+    /// <summary>
+    /// Existing position closed. API returns correct position.
+    /// </summary>
+    [Test]
+    [TestCase("PI_XBTUSD", "test", Contracts.Direction.Buy)]
+    [TestCase("PI_XBTUSD", "test", Contracts.Direction.Sell)]
+    public async Task EventRedelivered_ApiReturnsCorrectPosition(string symbol, string strategy, Contracts.Direction direction)
+    {
+        // Arrange.
+        await using var testEnvironment = new TestEnvironment();
+        var cancellationTokenSource = new CancellationTokenSource(testEnvironment.TestsSettings.ArrangeTimeoutMs);
+        await testEnvironment.SetupAsync(cancellationTokenSource.Token);
+        using var mediator = new OperationalDataStorageMediator(testEnvironment);
+        var oppositeDirection = direction == Contracts.Direction.Buy ? Contracts.Direction.Sell : Contracts.Direction.Buy;
+        var filledOrder = FilledOrderFactory.GenerateFilledOrder(symbol, strategy, direction, 100, 1000);
+        var eventsSnapshot = EventsSnapshotFactory.GenerateEventsSnapshot([filledOrder]);
+
+        // Act.
+        mediator.Publish(eventsSnapshot, mediator.EventsSnapshotSettings.ExchangeName, mediator.EventsSnapshotSettings.RoutingKeys);
+        mediator.Publish(eventsSnapshot, mediator.EventsSnapshotSettings.ExchangeName, mediator.EventsSnapshotSettings.RoutingKeys);
+        await Task.Delay(1000); // Wait for messages to be processed.
+        cancellationTokenSource = new CancellationTokenSource(testEnvironment.TestsSettings.ActTimeoutMs);
+        var positions = await mediator.GetPositionsAsync(strategy, cancellationTokenSource.Token);
+        await testEnvironment.StopAsync(CancellationToken.None);
+
+        // Assert.
+        positions.Should().NotBeNull();
+        positions.Positions.Should().NotBeNull();
         positions.Positions.Should().HaveCount(1);
         var position = positions.Positions.First();
 
-        var amount = filledOrder1.AverageFillPrice * filledOrder1.FilledQuantity - filledOrder2.AverageFillPrice * filledOrder2.FilledQuantity;
-        var averageFillPrice = amount / (filledOrder1.FilledQuantity - filledOrder2.FilledQuantity);
+        var amount = filledOrder.AverageFillPrice * filledOrder.FilledQuantity;
+        var averageFillPrice = amount / filledOrder.FilledQuantity;
         position.Should().NotBeNull();
         position.Symbol.Should().Be(symbol);
         position.Strategy.Should().Be(strategy);
-        position.Quantity.Should().Be(filledOrder1.FilledQuantity - filledOrder2.FilledQuantity);
+        position.Quantity.Should().Be(filledOrder.FilledQuantity);
         position.EntryPrice.Should().Be(averageFillPrice);
         position.Direction.Should().Be(direction);
-        position.OrderIds.Should().Contain(filledOrder1.OrderId);
-        position.OrderIds.Should().Contain(filledOrder2.OrderId);
+        position.OrderIds.Should().HaveCount(1);
+        position.OrderIds.Should().Contain(filledOrder.OrderId);
     }
 
     /// <summary>
@@ -212,11 +243,12 @@ public class ServiceTests
         using var mediator = new OperationalDataStorageMediator(testEnvironment);
         var oppositeDirection = direction == Contracts.Direction.Buy ? Contracts.Direction.Sell : Contracts.Direction.Buy;
         var filledOrder = FilledOrderFactory.GenerateFilledOrder(symbol, strategy, direction, 100, 1000);
-        var eventsSnapshot = EventsSnapshotFactory.GenerateEventsSnapshot([filledOrder]);
+        var eventsSnapshot1 = EventsSnapshotFactory.GenerateEventsSnapshot([filledOrder]);
+        var eventsSnapshot2 = EventsSnapshotFactory.GenerateEventsSnapshot([filledOrder]);
 
         // Act.
-        mediator.Publish(eventsSnapshot, mediator.EventsSnapshotSettings.ExchangeName, mediator.EventsSnapshotSettings.RoutingKeys);
-        mediator.Publish(eventsSnapshot, mediator.EventsSnapshotSettings.ExchangeName, mediator.EventsSnapshotSettings.RoutingKeys);
+        mediator.Publish(eventsSnapshot1, mediator.EventsSnapshotSettings.ExchangeName, mediator.EventsSnapshotSettings.RoutingKeys);
+        mediator.Publish(eventsSnapshot2, mediator.EventsSnapshotSettings.ExchangeName, mediator.EventsSnapshotSettings.RoutingKeys);
         await Task.Delay(1000); // Wait for messages to be processed.
         cancellationTokenSource = new CancellationTokenSource(testEnvironment.TestsSettings.ActTimeoutMs);
         var positions = await mediator.GetPositionsAsync(strategy, cancellationTokenSource.Token);
